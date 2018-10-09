fc08d80d287a9ce0516d7fb4838ca01cd4b2a09d
Thien Bao <baophamthien@gmail.com>
2018-10-09 13:14:03 +0700

    Code

---
+++ 04_Implement/02_Source/MyApplication/app/src/main/java/com/example/thienbao/myapplication/ChinhSuaActivity.java
@@ -0,0 +1,14 @@
+package com.example.thienbao.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ChinhSuaActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.custom_admindialog);
+    }
+}
